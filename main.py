9951a589bf2725686047f0b9b9365c419406fb22
Keller Scholl <keller.scholl@gmail.com>
2017-01-29 00:03:29 -0500

    Updates for compatibility with Python 3.5.2, specifically removing a bug where no files would be downloaded. Improves speed in cases where prior backup exists. Improves pythonic nature. Added several explicit error messages for edge cases. Added warning when pages fail to download for futureproofing. Improved commenting.

--- main.py
+++ main.py
@@ -1,70 +1,68 @@
 import re
-import sys
+from sys import argv
 from urllib.request import urlopen
 from os import mkdir
- 
+from os import listdir
+
 # Name of the tumblr.
-username = sys.argv[1]
- 
-# Download all the pages into a directory called Pages.
-#call(["mkdir", "Pages"])
-mkdir("Pages")
-next_page   = 1
-page_exists = True
-while page_exists:
-    #Figure out a way to replace this with urllib
-    u = urlopen("http://"+username+".tumblr.com/page/"+str(next_page))
-    with open("Pages/page"+str(next_page)+".html","wb") as f:
-        f.write(u.read())
-    #call(["wget", "http://"+username+".tumblr.com/page/"+str(next_page),\
-    #      "-O", "Pages/page"+str(next_page)+".html"])
-    page_exists = False
-    # Read through current page looking for link to next page,
-    # to see if it exists.
-    pg1 = re.compile("http://"+username+".tumblr.com/page/"+str(next_page+1))
-    pg2 = re.compile("<a href=\"/page/"+str(next_page+1)+"\">")
-    f  = open("Pages/page"+str(next_page)+".html", "r")
-    while True:
-        # Read line.
-        line = f.readline()
-        if not line:
-            break
-        # Search line for correct URL.
-        m1 = pg1.search(line)
-        m2 = pg2.search(line)
+username = argv[1]
+
+
+def getPosts(contents, username, posts):
+    pst1 = re.compile("http://"+username+".tumblr.com/post/([0-9]*)[^0-9]")
+    pst2 = re.compile("<a href=\"/post/([0-9]*)/")
+    for pst in [pst1, pst2]:
+        m = pst.findall(contents)
+        if len(m) > 0:
+            for num in m:
+                posts.add(num)
+    return posts
+
+if __name__ == "__main__":
+    incremental = False
+    try:
+        mkdir(username)
+        mkdir(username+"/Pages")
+        mkdir(username+"/Posts")
+    except FileExistsError:
+        try:
+            mkdir(username+"/Pages")
+            mkdir(username+"/Posts")
+            raise Exception(username+" is already a folder, but it wasn't made by this program.")
+        except FileExistsError:
+            incremental = True
+            print("Tumblr previously downloaded. Continuing download. Pages will reflect new arrangement.")
+    next_page   = 1
+    page_exists = True
+    posts       = set()
+    while page_exists:
+        # Figure out a way to replace this with urllib
+        u = urlopen("http://"+username+".tumblr.com/page/"+str(next_page))
+        with open(username+"/Pages/page"+str(next_page)+".html", "wb") as f:
+            h = u.read()
+            f.write(h)
+        page_contents = str(h)
+        # Read through current page looking for link to next page,
+        # to see if it exists.
+        pg1 = re.compile("http://"+username+".tumblr.com/page/"+str(next_page+1))
+        pg2 = re.compile("<a href=\"/page/"+str(next_page+1)+"\">")
+        m1 = pg1.search(page_contents)
+        m2 = pg2.search(page_contents)
         if m1 or m2:
             page_exists = True
-    f.close()
-    next_page += 1
- 
-# Go through all the pages.  Make a set of all the posts.
-posts = set()
-total_pages = next_page - 1
-pst1 = re.compile("http://"+username+".tumblr.com/post/([0-9]*)[^0-9]")
-pst2 = re.compile("<a href=\"/post/([0-9]*)/")
-# For each page:
-for page in range(total_pages):
-    f  = open("Pages/page"+str(page+1)+".html", "r")
-    # Scan page for mentions of posts.
-    while True:
-        # Read line.
-        line = f.readline()
-        if not line:
-            break
-        # Search line for mentions of posts.
-        m1 = pst1.findall(line)
-        m2 = pst2.findall(line)
-        if len(m1) > 0:
-            for num in m1:
-                posts.add(num)
-        if len(m2) > 0:
-            for num in m2:
-                posts.add(num)
-    f.close()
- 
-# Download each post identified.
-mkdir("Posts")
-for p in posts:
-    u = urlopen("http://"+username+".tumblr.com/post/"+p)
-    with open("Posts/"+p+".html","wb") as f:
-        f.write(u.read())
+        else:
+            page_exists = False
+        posts = getPosts(page_contents, username, posts)
+        next_page += 1
+        if next_page > 10000:
+            raise Exception("Program is not terminating on "+username+" and has collected 10000 pages already.")
+    if next_page < 3:
+        print("Probable bug: less than two pages saved.")
+        print(username)
+    # Download each post identified.
+    existing = os.listdir(username+"/Posts")
+    difference = [m for m in posts if m not in existing]
+    for p in existing:
+        u = urlopen("http://"+username+".tumblr.com/post/"+p)
+        with open(username+"/Posts/"+p+".html", "wb") as f:
+            f.write(u.read())
